a0b4207ba04367bef9bab66925a1351b8b13a0a6
Felix Holguin <fholguinj@gmail.com>
2013-06-04 15:36:29 -0500

    ListaDistritos

--- app/models/local.rb
+++ app/models/local.rb
@@ -3,7 +3,8 @@ class Local < ActiveRecord::Base
   acts_as_gmappable
 
 def gmaps4rails_address
+$global_variable = 'Lima'
 #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-  "#{address}" 
+    "#{address}, #{district}, #{$global_variable}"
 end
 end
