9a2a8857416bd60cf1a3b7061c708aa0056a6b7e
Jiam <jiam.jual@gmail.com>
2013-06-05 00:11:50 -0500

    HU03 - Validaciones para registro de grupos musicales.

--- app/models/musical_group.rb
+++ app/models/musical_group.rb
@@ -3,4 +3,11 @@ class MusicalGroup < ActiveRecord::Base
  	belongs_to :music_style
   	has_many :user_musical_groups
   	has_many :users, :through => :user_musical_groups
+
+  	#Validaciones
+  	validates 	:name, :presence => {:message => "El nombre no puede estar en blanco."},
+  				:uniqueness => {:message => "Grupo musical ya existe."}
+
+  	validates 	:music_style_id, :presence => {:message => "Debe elegir un estilo musical."}
+  	validates 	:user_ids, :presence => {:message => "Debe elegir al menos un miembro para el grupo."}
 end
